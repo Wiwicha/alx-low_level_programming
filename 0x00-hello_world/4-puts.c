1c3dff2104ea802a1829b0ee1fa7ea0dc43ff9ef
wiam <wiam.silabdi.eln@gmail.com>
2023-08-10 15:00:20 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descreption: A C program that prints with puts function
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
