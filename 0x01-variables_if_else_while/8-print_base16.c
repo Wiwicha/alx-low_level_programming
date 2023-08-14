5de9f2c8d7d326450caa9ac805611f419bc8e2b8
wiam <wiam.silabdi.eln@gmail.com>
2023-08-13 18:08:00 -0700

    c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 48; /*48 decimal representation of 0*/
+
+	while (digit <= 102) /*102 is the decimal representation of f*/
+	{
+		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
+		digit++
+	}
+	putchar('\n');
+	return (0);
+}
