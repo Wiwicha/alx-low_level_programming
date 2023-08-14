4d977f813580b1bccbafbd21ad112558ea7f35dc
wiam <wiam.silabdi.eln@gmail.com>
2023-08-13 17:37:38 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
