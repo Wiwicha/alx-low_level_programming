e7a884a2ea2aa97d6b931ae7baee986191edb8c2
wiam <wiam.silabdi.eln@gmail.com>
2023-08-15 15:00:39 -0700

    n

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -6,7 +6,7 @@
  * @c: the character to print
  *
  * Return: On success 1.
- *         On erroe, -1 is returned. and eerno is set appropriately.
+ *         On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
 
