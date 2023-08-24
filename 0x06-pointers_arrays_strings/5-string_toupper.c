51e083689e1482d9d4bb0ccbcd2826bee2032e71
wiam <wiam.silabdi.eln@gmail.com>
2023-08-24 09:55:54 -0700

    n

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *string_toupper - a function that changes all lowercase letters of a string to upper
+ * *string_toupper - function changes all lower letters of a string to upper
  * @str: string
  * Return: string
 */
