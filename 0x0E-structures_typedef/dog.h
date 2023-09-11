daa1fed90ff2f5e3bec30d5c0380c54cdcb67e6e
wiam <wiam.silabdi.eln@gmail.com>
2023-09-11 15:21:39 -0700

    n

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct - dog's identity
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: poppy poppy dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-readme
+ C - Structures, typedef
